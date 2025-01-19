3cae392727df98e90ce70f0d538032407d1aefdf
Aline-Aguiar <aline.aglilly@gmail.com>
2025-01-18 21:04:05 -0300

    refatorando código do index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const numeros = [65, 44, 12, 4]
 
-numeros.forEach(function(item,indice,array){
-    let numero = numeros[indice]*10
-    console.log(numero)
+numeros.forEach(function(item){
+    console.log(item*10)
 })
